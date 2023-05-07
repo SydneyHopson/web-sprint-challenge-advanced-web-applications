4d993b902e1d93e21f25582082470304ebd73cbf
Sydney Hopson <sydneyhopsonreignmobile@Sydneys-iMac.local>
2023-05-07 08:41:13 -0400

    MVP almost finished fixing upda te article wont update just creates a new article

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,15 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { NavLink, Routes, Route, useNavigate } from 'react-router-dom'
 import Articles from './Articles'
 import LoginForm from './LoginForm'
 import Message from './Message'
 import ArticleForm from './ArticleForm'
 import Spinner from './Spinner'
+import axios from 'axios'
+import  axiosWithAuth from '../axios'
+
+
+
 
 const articlesUrl = 'http://localhost:9000/api/articles'
 const loginUrl = 'http://localhost:9000/api/login'
@@ -16,12 +21,27 @@ export default function App() {
   const [currentArticleId, setCurrentArticleId] = useState()
   const [spinnerOn, setSpinnerOn] = useState(false)
 
+  useEffect(()=> {
+    setCurrentArticleId(articles.find(art => art.article_id === currentArticleId))
+  }, [currentArticleId])
+
+  
+
   // ✨ Research `useNavigate` in React Router v.6
   const navigate = useNavigate()
-  const redirectToLogin = () => { /* ✨ implement */ }
-  const redirectToArticles = () => { /* ✨ implement */ }
+  const redirectToLogin = () => navigate('/')
+  const redirectToArticles = () =>  navigate('/articles')
+
+  const resetArticles = () => {
+    axiosWithAuth().get("articles")
+    .then(res => setArticles(res.data.articles))
+  }
 
   const logout = () => {
+    localStorage.removeItem("token");
+    setMessage("Goodbye!");
+    redirectToLogin()
+
     // ✨ implement
     // If a token is in local storage it should be removed,
     // and a message saying "Goodbye!" should be set in its proper state.
@@ -30,6 +50,18 @@ export default function App() {
   }
 
   const login = ({ username, password }) => {
+    setMessage("");
+    setSpinnerOn(true);
+     axios.post(loginUrl, {username: username.trim(), password: password.trim() }).then(res => {
+      console.log(res)
+      localStorage.setItem("token", res.data.token);
+      setMessage(res.data.message);
+      setSpinnerOn(false);
+      redirectToArticles()
+    })
+    .catch(err => console.log(err))
+
+
     // ✨ implement
     // We should flush the message state, turn on the spinner
     // and launch a request to the proper endpoint.
@@ -39,6 +71,21 @@ export default function App() {
   }
 
   const getArticles = () => {
+    setMessage("");
+    setSpinnerOn(true);
+    axiosWithAuth()
+    .get("/articles")
+    .then(res => {
+      setArticles(res.data.articles)
+      setSpinnerOn(false)
+      setMessage(res.data.message)
+    })
+    .catch(err => {
+      if (err.status === 401) {
+        redirectToLogin()
+      }
+      setSpinnerOn(false)
+    })
     // ✨ implement
     // We should flush the message state, turn on the spinner
     // and launch an authenticated request to the proper endpoint.
@@ -49,7 +96,17 @@ export default function App() {
     // Don't forget to turn off the spinner!
   }
 
-  const postArticle = article => {
+   const postArticle = article => {
+    setMessage("")
+    axiosWithAuth()
+    .post(articlesUrl, article)
+    .then((res) => {
+      const newArt = [...articles]
+      newArt.push({...article, article_id: Date.now()})
+      setArticles(newArt)
+      setMessage(res.data.message)
+    })
+      .catch(err => console.log(err))
     // ✨ implement
     // The flow is very similar to the `getArticles` function.
     // You'll know what to do! Use log statements or breakpoints
@@ -57,19 +114,42 @@ export default function App() {
   }
 
   const updateArticle = ({ article_id, article }) => {
+    axiosWithAuth().put(`articlesUrl/${article_id}`,  article)
+    .then(res => {
+        console.log("HERE",res)
+      setMessage(res.data.message)
+      setArticles(articles.map(art => { if(art.articles_id === res.data.id)
+        return res.data.article 
+       }))
+      // resetArticles()
+    })
+    .catch(err => console.log(err));
     // ✨ implement
     // You got this!
   }
 
   const deleteArticle = article_id => {
     // ✨ implement
+    setMessage('');
+    setSpinnerOn(true);
+    axiosWithAuth()
+      .delete(`/articles/${article_id}`)
+      .then(res => {
+        setMessage(res.data.message)
+        setArticles(articles.filter((update) => update.article_id !== article_id));
+        setSpinnerOn(false);
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    // ✨ implement
   }
 
   return (
     // ✨ fix the JSX: `Spinner`, `Message`, `LoginForm`, `ArticleForm` and `Articles` expect props ❗
     <>
-      <Spinner />
-      <Message />
+      <Spinner on={spinnerOn}/>
+      <Message message={message} />
       <button id="logout" onClick={logout}>Logout from app</button>
       <div id="wrapper" style={{ opacity: spinnerOn ? "0.25" : "1" }}> {/* <-- do not change this line */}
         <h1>Advanced Web Applications</h1>
@@ -78,11 +158,11 @@ export default function App() {
           <NavLink id="articlesScreen" to="/articles">Articles</NavLink>
         </nav>
         <Routes>
-          <Route path="/" element={<LoginForm />} />
-          <Route path="articles" element={
+          <Route path="/" element={<LoginForm  login={login}/>} />
+          <Route path="/articles" element={
             <>
-              <ArticleForm />
-              <Articles />
+              <ArticleForm currentArticle={currentArticleId} postArticle={postArticle} updateArticle={updateArticle} setCurrentArticleId={setCurrentArticleId}/>
+              <Articles articles={articles} getArticles={getArticles} setCurrentArticleId={setCurrentArticleId} deleteArticle={deleteArticle}/>
             </>
           } />
         </Routes>
